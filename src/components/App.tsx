4514e8039718f1ee7961f26f2f2b3c443d687c2f
Alan Lamas <alan12394@hotmail.com>
2023-01-08 23:42:50 -0300

    updated styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import '../App.css';
 
 import appStoreIcon from '../src/assets/images/appstore.png';
 import facebookIcon from '../src/assets/images/Facebook.png';
@@ -12,6 +12,16 @@ function App() {
     <div className="App">
       <header className="App-header">
       </header>
+      <body>
+        <div className="main-section">
+          <div className="right-side">
+            Right
+          </div>
+          <div className="left-side">
+            Left
+          </div>
+        </div>
+      </body>
     </div>
   );
 }
