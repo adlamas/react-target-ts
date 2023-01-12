20eec961fb9bcbd168f16d3322c4e97a532997a8
Alan Lamas <alan12394@hotmail.com>
2023-01-12 16:53:35 -0300

    refactored component

--- src/components/signUp/Form.tsx
+++ src/components/signUp/Form.tsx
@@ -1,46 +1,65 @@
+import { Component } from 'react';
+import { render } from 'react-dom';
 
-export default function form() {
-  return ( 
-    <form action="http://localhost:3001/api/v1/users" method="POST">
-      <p className="input-title">SIGN UP</p>
+export default class SignUp extends Component {
+  constructor(props: any) {
+    super(props);
+  }
 
-      <div className="input-element">
-        <label htmlFor="name">NAME</label>
-        <input type="text" name="name" />
-      </div>
+  printEmail(form: any) {
+    debugger;
+    console.log(form);
+  }
 
-      <div className="input-element">
-        <label htmlFor="email">EMAIL</label>
-        <input type="email" name="email" />
-      </div>
+  render(){
+    return (
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          this.printEmail(e.target);
+        }}
+      >
+        <p className="input-title">SIGN UP</p>
 
-      <div className="input-element">
-        <label htmlFor="password">PASSWORD</label>
-        <input type="password" name="password" placeholder="MIN. 6 CHARACTERS LONG" />
-      </div>
+        <div className="input-element">
+          <label htmlFor="name">NAME</label>
+          <input type="text" name="name" />
+        </div>
 
-      <div className="input-element">
-        <label htmlFor="confirm-password">CONFIRM PASSWORD</label>
-        <input type="text" name="confirm-password" />
-      </div>
+        <div className="input-element">
+          <label htmlFor="email">EMAIL</label>
+          <input type="email" name="email" />
+        </div>
 
-      <div className="input-element">
-        <label htmlFor="gender">GENDER</label>
-        <select name="gender">
-          <option value="select-your-gender">SELECT YOUR GENDER</option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-          <option value="not-to-say">Prefer not to say</option>
-        </select>
-      </div>
+        <div className="input-element">
+          <label htmlFor="password">PASSWORD</label>
+          <input type="password" name="password" placeholder="MIN. 6 CHARACTERS LONG" />
+        </div>
 
-      <div className="input-element">
-        <input type="submit" name="sign-up" value="SIGN UP" />
-      </div>
+        <div className="input-element">
+          <label htmlFor="confirm-password">CONFIRM PASSWORD</label>
+          <input type="text" name="confirm-password" />
+        </div>
 
-      <div className="sign-in-section" >
-        <p className='sign-in-text'>SIGN IN</p>
-      </div>
-    </form>
-  )
+        <div className="input-element">
+          <label htmlFor="gender">GENDER</label>
+          <select name="gender">
+            <option value="select-your-gender">SELECT YOUR GENDER</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="not-to-say">Prefer not to say</option>
+          </select>
+        </div>
+
+        <div className="input-element">
+          <input type="submit" name="sign-up" value="SIGN UP" />
+        </div>
+
+        <div className="sign-in-section" >
+          <p className='sign-in-text'>SIGN IN</p>
+        </div>
+      </form>
+    )
+  }
 }
+
