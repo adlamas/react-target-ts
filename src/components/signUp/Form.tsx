39a60d91664b2440983562d516ce5dd377ced80e
Alan Lamas <alan12394@hotmail.com>
2023-01-12 23:42:55 -0300

    added axios package, updated component to retrieve inputs

--- src/components/signUp/Form.tsx
+++ src/components/signUp/Form.tsx
@@ -1,14 +1,50 @@
+import axios from 'axios';
 import { Component } from 'react';
 import { render } from 'react-dom';
+import { useState } from 'react';
+
 
 export default class SignUp extends Component {
+  username: string;
+  email: string;
+  password: string;
+  passwordConfirmation: string;
+  gender: string;
+
   constructor(props: any) {
     super(props);
+    this.email = '';
+    this.password = '';
+    this.passwordConfirmation = '';
+    this.gender = '';
+    this.username = '';
   }
 
-  printEmail(form: any) {
-    debugger;
-    console.log(form);
+  async sendSignUp() {
+    try {
+      debugger;
+      const res = await axios.post(
+        'http://localhost:3001/api/v1/users',
+        { user:
+          {
+            email: this.email,
+            password: this.password,
+            password_confirmation: this.passwordConfirmation,
+            gender: this.gender
+          }
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json'
+          }}
+        );
+
+      debugger;
+      return res;
+    }
+    catch(e){
+      console.log(e)
+    }
   }
 
   render(){
@@ -16,34 +52,65 @@ export default class SignUp extends Component {
       <form
         onSubmit={e => {
           e.preventDefault();
-          this.printEmail(e.target);
+          return this.sendSignUp();
         }}
       >
         <p className="input-title">SIGN UP</p>
 
         <div className="input-element">
           <label htmlFor="name">NAME</label>
-          <input type="text" name="name" />
+          <input
+            type="text"
+            name="name"
+            onChange={e => {
+              this.username = e.target.value;
+              console.log(this.username);
+            }}
+          />
         </div>
 
         <div className="input-element">
           <label htmlFor="email">EMAIL</label>
-          <input type="email" name="email" />
+          <input 
+            type="email"
+            name="email"
+            onChange={e => {
+              this.email = e.target.value;
+              console.log(this.email);
+            }}
+          />
         </div>
 
         <div className="input-element">
           <label htmlFor="password">PASSWORD</label>
-          <input type="password" name="password" placeholder="MIN. 6 CHARACTERS LONG" />
+          <input
+            type="password"
+            name="password"
+            placeholder="MIN. 6 CHARACTERS LONG"
+            onChange={e => {
+              this.password = e.target.value;
+            }}
+          />
         </div>
 
         <div className="input-element">
           <label htmlFor="confirm-password">CONFIRM PASSWORD</label>
-          <input type="text" name="confirm-password" />
+          <input
+            type="password"
+            name="confirm-password"
+            onChange={e => {
+            this.passwordConfirmation = e.target.value;
+            }}
+          />
         </div>
 
         <div className="input-element">
           <label htmlFor="gender">GENDER</label>
-          <select name="gender">
+          <select name="gender"
+            onChange={e => {
+            this.gender = e.target.value;
+            }}          
+          >
             <option value="select-your-gender">SELECT YOUR GENDER</option>
             <option value="male">Male</option>
             <option value="female">Female</option>
