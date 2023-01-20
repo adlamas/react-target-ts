fe6c8e061d3d5e89c28f29874dcaf12a6fa5bbd8
Alan Lamas <alan12394@hotmail.com>
2023-01-20 15:13:33 -0300

    layout left panel of home pages

--- src/components/home/welcome/LeftPanel.tsx
+++ src/components/home/welcome/LeftPanel.tsx
@@ -1,20 +1,40 @@
 import smiliesIcon from 'assets/images/smilies.png'
+import ovalIcon from 'assets/images/oval.png'
 
 const leftPanel = () => {
   return(
-    <div>
-      <img src={smiliesIcon} alt="Two faces smiling" />
+    <div style={{width: '80%', flexDirection: 'column'}}>
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <img src={smiliesIcon} alt="Two faces smiling" style={{width: '30%'}} />
+      </div>
 
-      <p>Welcome to TARGET </p>
-      <p>Find people near you & Connect</p>
-      
-      <p>
-        Create a  target  by clicking wherever on the map, specify  the ratio and and a topic: Travel, Dating, Music, etc.
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <p style={{textAlign: 'center'}}>Welcome to TARGET </p>
+      </div>
+
+      <div style={{display: 'flex', justifyContent: 'center'}}>
+        <p style={{textAlign: 'center'}}>Find people near you & Connect</p>
+      </div>
+        
+      <p style={{textAlign: 'left', display: 'flex'}}>
+        <div style={{marginRight: '0.2em'}}>
+          <img src={ovalIcon} alt="Oval icon" />
+        </div>
+        <div>
+          Create a  target  by clicking wherever on the map, specify  the ratio and and a topic: Travel, Dating, Music, etc.
+        </div>
+      </p>
+
+      <p style={{textAlign: 'left', display: 'flex'}}>
+        <div style={{marginRight: '0.2em'}}>
+          <img src={ovalIcon} alt="Oval icon" />
+        </div>
+        <div>
+          TARGET will  start a chat whenever you’ve a match. You can always dismiss a conversation if you’re not interested.
+        </div>
       </p>
       
-      <p>TARGET will  start a chat whenever you’ve a match.  You can always dismiss a conversation if you’re not interested.</p>
-
-      <p>
+      <p style={{textAlign: 'center'}}>
         <input type='button' value='Ok, got it!'/>
       </p>
     </div>
